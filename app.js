9af1e4ce99ec134f0c6af78b1fc0a8f0718a5c14
Siddharth Ramesh <sramesh@gainsight.com>
2017-09-02 19:01:44 +0530

    Explicit mount method

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ import Header from './src/widgets/header/header.component'
 import Feed from './src/widgets/feed/feed.component';
 import Status from './src/widgets/status/status.component';
 
+class App {
+  constructor() {
+    this.init();
+  }
 
+  mountComponents() {
+    new Header();
+    new Status();
+    new Feed().showFeed();
+  }
+
+  init() {
+    this.mountComponents();
+  }
+
+}
 $(document).ready(function() {
-  new Header();
-  new Status();
-  new Feed().showFeed();
+  new App();
 });
